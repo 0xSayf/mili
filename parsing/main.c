4d5d60970d98e464d4f32063fe54ffcb4387857d
Sayf-Eddine Hamzaoui <sahamzao@c1r8p6.1337.ma>
2024-11-22 21:44:36 +0100

    sa

--- parsing/main.c
+++ parsing/main.c
@@ -6,7 +6,7 @@
 /*   By: sahamzao <sahamzao@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 12:49:18 by sahamzao          #+#    #+#             */
-/*   Updated: 2024/11/22 21:36:17 by sahamzao         ###   ########.fr       */
+/*   Updated: 2024/11/22 21:44:21 by sahamzao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,7 +67,7 @@ int main (int ac , char **av, char **env)
         sir = ft_init_token_node(token);
         ft_geave_type(sir,env);
         ft_syntax(sir);
-        // ft_initial_env(evv,env);
+        ft_initial_env(evv,env);
         ft_lstclear(&sir,&del);
         free(token);
         
