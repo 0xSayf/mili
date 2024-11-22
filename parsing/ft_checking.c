a243ace240d93eb1cd3fbf38f9df8a86f37c32d1
Sayf-Eddine Hamzaoui <sahamzao@c1r2p6.1337.ma>
2024-11-22 15:37:05 +0100

    sa

--- parsing/ft_checking.c
+++ parsing/ft_checking.c
@@ -6,7 +6,7 @@
 /*   By: sahamzao <sahamzao@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 16:53:52 by sahamzao          #+#    #+#             */
-/*   Updated: 2024/11/22 11:07:45 by sahamzao         ###   ########.fr       */
+/*   Updated: 2024/11/22 15:34:54 by sahamzao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -108,13 +108,18 @@ void	ft_syntax(t_token *syntax)
 	t_token *tmp;
 
 
+	tmp = syntax;
 	i = 0;
 	if(!i && *(syntax->string) == '|')
 	{
 		printf("syntax error\n");
 		return ;
 	}
-	tmp = syntax;
+	else if (!i && tmp->typ_e == STRING)
+	{
+		printf("Syntax Error : cmd not found \n");
+		return ;
+	}
 	while (tmp)
 	{
 		if(tmp->typ_e == PIPE && !tmp->next)
