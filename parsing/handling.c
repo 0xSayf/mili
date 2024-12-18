88a0d2ad33db72a472dfdb61f857c04954c6fa1d
Sayf-Eddine Hamzaoui <sahamzao@c1r5p3.1337.ma>
2024-12-18 17:37:50 +0100

    sa'g

--- parsing/handling.c
+++ parsing/handling.c
@@ -6,7 +6,7 @@
 /*   By: sahamzao <sahamzao@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 12:55:51 by sahamzao          #+#    #+#             */
-/*   Updated: 2024/12/18 17:08:27 by sahamzao         ###   ########.fr       */
+/*   Updated: 2024/12/18 17:37:21 by sahamzao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@ t_red    *ft_check_redirection(t_cmd  *cmd, t_token *token)
     red = cmd->redirection;
     while(token)
     {
+        if(token->typ_e == PIPE)
+            break;
         if(token->typ_e == REDERECTION_OUTPUT || token->typ_e == APPEND_REDIRECT)
         {
             if(!red)
@@ -33,7 +35,6 @@ t_red    *ft_check_redirection(t_cmd  *cmd, t_token *token)
             {
             help->next = ft_creat_redirection_node(token->next,token->typ_e);
             help = help->next;
-            // printf("%s\n", help->red_append);
             }
         }
         token = token->next;
