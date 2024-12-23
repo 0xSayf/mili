c2fe61e1017b3dc31ae78f2d7d05c15afcb421a3
Sayf-Eddine Hamzaoui <sahamzao@c1r2p6.1337.ma>
2024-12-23 12:10:04 +0100

    sa

--- utils/creat_node_cmd.c
+++ utils/creat_node_cmd.c
@@ -6,7 +6,7 @@
 /*   By: sahamzao <sahamzao@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 20:57:46 by sahamzao          #+#    #+#             */
-/*   Updated: 2024/12/23 11:58:08 by sahamzao         ###   ########.fr       */
+/*   Updated: 2024/12/23 12:09:09 by sahamzao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,8 +81,6 @@ t_red	*ft_cr_redir_node(t_token *token, enum e_data_type typ_e)
 	t_red	*red;
 
 	red = malloc(sizeof(t_red));
-	if (!red)
-		return (NULL);
 	red->typix = typ_e;
 	if (typ_e == REDERECTION_OUTPUT)
 	{
