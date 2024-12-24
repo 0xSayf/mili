82610cedebae0d97a2e2f075b9be86f5e778db45
Sayf-Eddine Hamzaoui <sahamzao@c2r8p4.1337.ma>
2024-12-24 15:37:44 +0100

    s

--- parsing/dollar.c
+++ parsing/dollar.c
@@ -6,7 +6,7 @@
 /*   By: sahamzao <sahamzao@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/24 13:45:23 by sahamzao          #+#    #+#             */
-/*   Updated: 2024/12/24 15:34:05 by sahamzao         ###   ########.fr       */
+/*   Updated: 2024/12/24 15:37:04 by sahamzao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,29 +48,32 @@ t_token *ft_expand_dollar(t_token *token,t_env  *env)
     char    *ptr;
     char    *str;
     
-    if(!token || !token->string || !env)
+    if(!token || !env)
         return NULL;
     tmp = token;
     while (tmp)
     {
-        ptr = tmp->string;
-        if (ptr[0] == '$' && !ptr[1])
+        if (tmp->string)
         {
-            free(tmp->string);
-            tmp->string = malloc(1);
-            tmp->string[0] = '\0';           
-        }
-        else if (ptr[0] == '$' && ptr[1])
-        {
-            str = ft_search(&ptr[1],env);
-            if(str)
+            ptr = tmp->string;
+            if (ptr[0] == '$' && !ptr[1])
             {
                 free(tmp->string);
-                tmp->string = malloc(ft_strlen(str + 1) + 1);
-                ft_memcpy(tmp->string,str + 1,ft_strlen(str + 1));
-                tmp->string[ft_strlen(str+ 1)] = '\0';
+                tmp->string = malloc(1);
+                tmp->string[0] = '\0';           
             }
-            // printf("%s\n", tmp->string);   
+            else if (ptr[0] == '$' && ptr[1])
+            {
+                str = ft_search(&ptr[1],env);
+                if(str)
+                {
+                    free(tmp->string);
+                    tmp->string = malloc(ft_strlen(str + 1) + 1);
+                    ft_memcpy(tmp->string,str + 1,ft_strlen(str + 1));
+                    tmp->string[ft_strlen(str+ 1)] = '\0';
+                }
+                // printf("%s\n", tmp->string);   
+            }
         }
         tmp = tmp->next;
     }
